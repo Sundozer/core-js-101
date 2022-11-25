92642fc4bd6fec5f5c2229ba6fa99ab8f67138ae
Alex Hramovich <xtr1m@mail.ru>
2022-11-25 15:47:00 +0300

    feat: add other new tasks

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -58,8 +58,8 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(proto, json) {
-  
+function fromJSON(/* proto, json */) {
+  throw new Error('Not implemented');
 }
 
 
